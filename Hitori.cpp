437dadd6c20247b96f3c9500df0fb3ff85037d81
soha4ever <hitomistar99@gmail.com>
2017-12-19 16:20:59 +0330

    everything has changed

--- Hitori.cpp
+++ Hitori.cpp
@@ -10,13 +10,19 @@ using namespace std;
 struct node
 {
 	short int value;//between 1 to 9
-	char condition;//W for white & B for black 
+	char condition;//W for white & B for black & N for non that means the node was not checked yet
 };
 //matrix initialed to be solved -> hitori main table
 struct state
 {
 	int size;
 	vector <vector<node> > matrix;
+};
+//struct used in CSP and AC3
+struct csp_struct {
+	int x;
+	int y;
+	vector<char> domine = {'W','B','N'};
 };
 //H function-> result shows how 
 int heuristic(state s)
@@ -38,17 +44,128 @@ int heuristic(state s)
 	}
 	return result;
 }
-//knadidates all the possible nodes that can become black in next moves 
-vector<state> successor(state s, short int level)
+
+//Rule #1(is goal in level): No number appears in a row or column more than once
+bool check_rule_one(state s, int level)
+{
+	int array[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+	for (int i = 0; i < s.size; i++)
+	{
+		node current = s.matrix[i][level];
+		if (current.condition == 'W')
+		{
+			if (array[current.value - 1] != 0)
+			{
+				return false;
+
+			}
+			else
+			{
+				array[current.value - 1] ++;
+			}
+		}
+	}
+	int array[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	for (int i = 0; i < s.size; i++)
+	{
+		node current = s.matrix[level][i];
+		if (current.condition == 'W')
+		{
+			if (array[current.value - 1] != 0)
+			{
+				return false;
+
+			}
+			else
+			{
+				array[current.value - 1] ++;
+			}
+		}
+	}
+	return true;
+}
+//Rule #2: black nodes do not touch each other vertically or horizontally
+bool check_rule_two(state s, int x, int y)
+{
+	
+	bool result=true;
+	if (x > 0)
+	{
+		if (s.matrix[x - 1][y].condition == 'B')
+		{
+			result = false;
+			return result;
+		}
+	}
+	if (x < s.size -1)
+	{
+		if (s.matrix[x - 1][y].condition == 'B')
+		{
+			result = false;
+			return result;
+		}
+	}
+	if (y > 0)
+	{
+		if (s.matrix[x][y - 1].condition == 'B')
+		{
+			result = false;
+			return result;
+		}
+	}
+	if (y < s.size - 1)
+	{
+		if (s.matrix[x][y + 1].condition == 'B')
+		{
+			result = false;
+			return result;
+		}
+	}
+	return result;
+
+}
+
+//Rule #3:all un-shaded (white) squares create a single continuous area T_T
+bool check_rule_three(state s, int x, int y)
+{
+
+}
+//checks if the chosen node's value is repeated
+bool repeated(state s,int x,int y)
+{
+	node K = s.matrix[x][y];
+	for (int i = 0; i < s.size; i++)
+	{
+		node current = s.matrix[i][y];
+		if (current.value == K.value)
+		{
+			return false;
+
+		}
+	}
+	for (int j = 0; j < s.size; j++)
+			{
+				node current = s.matrix[x][j];
+				if (current.value==K.value)
+				{
+					return false;
+				
+				}
+			}
+	return true;
+}
+//returns all the possible states in next moves 
+vector<state> successor(state s, int x,int y)
 {
 	vector<state> vec;
-	int array[8] = { -1, -1, -1, -1, -1, -1, -1, -1 };
+	/*int array[8] = { -1, -1, -1, -1, -1, -1, -1, -1 };
 	for (int i = 0; i < s.size; i++)
 	{
 		state temp = s;
 		
 		node current = temp.matrix[i][level];
-		if (current.condition == 'W')
+		if (current.condition == 'N')
 		{
 			if (array[current.value - 1] >=0)
 			{
@@ -100,142 +217,22 @@ vector<state> successor(state s, short int level)
 		{
 			array[current.value - 1] = i;
 		}
+	}*/
+	state temp = s;
+	if (check_rule_one(s, x) == true && check_rule_one(s, y) == true)
+	{
+		temp.matrix[x][y].condition = 'W';
+		vec.push_back(temp);
 	}
+	if (check_rule_two(s, x, y) == true && repeated(s,x,y)==true&& check_rule_three(s,x,y)==true)
+	{
+		temp.matrix[x][y].condition = 'B';
+		vec.push_back(temp);
+	}
+
 	return vec;
 }
-bool check_rule_two(state s, int x, int y)
-{
-	//Rule #2: black nodes do not touch each other vertically or horizontally
-	bool result=true;
-	if (x > 0)
-	{
-		if (s.matrix[x - 1][y].condition == 'B')
-		{
-			result = false;
-			return result;
-		}
-	}
-	if (x < s.size -1)
-	{
-		if (s.matrix[x - 1][y].condition == 'B')
-		{
-			result = false;
-			return result;
-		}
-	}
-	if (y > 0)
-	{
-		if (s.matrix[x][y - 1].condition == 'B')
-		{
-			result = false;
-			return result;
-		}
-	}
-	if (y < s.size - 1)
-	{
-		if (s.matrix[x][y + 1].condition == 'B')
-		{
-			result = false;
-			return result;
-		}
-	}
-	
 
-}
-
-//Rule #3:all un-shaded (white) squares create a single continuous area T_T
-bool check_rule_three(state s, int x, int y)
-{
-
-}
-//Rule #1(is goal in level): No number appears in a row or column more than once
-bool check_rule_one(state s, int level)
-{
-	int array[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-	for (int i = 0; i < s.size; i++)
-	{
-		node current = s.matrix[i][level];
-		if (current.condition == 'W')
-		{
-			if (array[current.value - 1] != 0)
-			{
-				return false;
-
-			}
-			else
-			{
-				array[current.value - 1] ++;
-			}
-		}
-	}
-	int array[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-
-	for (int i = 0; i < s.size; i++)
-	{
-		node current = s.matrix[level][i];
-		if (current.condition == 'W')
-		{
-			if (array[current.value - 1] != 0)
-			{
-				return false;
-
-			}
-			else
-			{
-				array[current.value - 1] ++;
-			}
-		}
-	}
-	return true;
-}
-vector<pair<int,int>> repeated(state s)
-{
-	vector<pair<int, int>> result;
-	int check[8][3] =
-	{
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-	};
-	for (int i = 0; i < s.size; i++)
-	{
-		for (int j = 0; j < s.size; j++)
-		{
-			node current = s.matrix[i][j];
-			if (check[current.value][0] == -1)
-			{
-				check[current.value][0] ++;
-				check[current.value][1] = i;
-				check[current.value][2] = j;
-				
-			}
-			else
-			{
-				check[current.value][0] ++;
-				pair<int, int> temp;
-				temp.first = i;
-				temp.second = j;
-				result.push_back(temp);
-			}
-		}
-	}
-	for (int i = 0; i < 8; i++)
-	{
-		if (check[i][0] > 0)
-		{
-			pair<int, int> temp;
-			temp.first = check[i][1];
-			temp.second = check[i][2];
-			result.push_back(temp);
-		}
-	}
-	return result;
-}
 
 state hill_climbing(state)
 {
@@ -244,4 +241,12 @@ state hill_climbing(state)
 state simulated_annaling(state)
 {
 
+}
+void AC3(state &s, vector<csp_struct> &csp_)
+{
+	for (int i = 0; i < csp_.size(); i++)
+	{
+		if (check_rule_one(s,csp_[i].x)==false)
+			csp_[i].
+	}
 }
